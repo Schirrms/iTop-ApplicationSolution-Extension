685e4cb40517d7179e3f999cf375eeb60acb6c99
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-02-23 15:59:26 +0100

    First upload after complete rework (v0.1.0)

---
+++ schirrms-as-addfields/en.dict.schirrms-as-addfields.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:ApplicationSolution/Attribute:as_cos' => 'Contract of service',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:247' => '24 hours / 24 - 7 days / 7',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:715' => 'Monday to Friday, 7h15 to 19h15',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:830' => 'Monday to Friday, 8h30 to 18h30',
+	'Class:ApplicationSolution/Attribute:as_domain' => 'Functional domain',
+	'Class:ApplicationSolution/Attribute:as_end_date' => 'Service end date',
+	'Class:ApplicationSolution/Attribute:as_last_update' => 'Last update date',
+	'Class:ApplicationSolution/Attribute:as_nature' => 'Nature',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:mixed' => 'Mixed',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:partner' => 'Partner',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:package' => 'Software Package',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:specific' => 'Specific',
+	'Class:ApplicationSolution/Attribute:as_state' => 'State',
+	'Class:ApplicationSolution/Attribute:as_state/Value:undefined' => 'Unclear',
+	'Class:ApplicationSolution/Attribute:as_state/Value:decommissioning' => 'Under decommissioning',
+	'Class:ApplicationSolution/Attribute:as_state/Value:decommissioned' => 'Decommissioned',
+	'Class:ApplicationSolution/Attribute:as_state/Value:production' => 'Production',
+	'Class:ApplicationSolution/Attribute:as_state/Value:external' => 'External',
+	'Class:ApplicationSolution/Attribute:as_state/Value:future' => 'Future',
+	'Class:ApplicationSolution/Attribute:as_type' => 'Usage type',
+	'Class:ApplicationSolution/Attribute:as_type/Value:job' => 'Business',
+	'Class:ApplicationSolution/Attribute:as_type/Value:technical' => 'Technical',
+	'Class:ApplicationSolution/Attribute:as_type/Value:a10' => 'Sur A10',
+	'Class:ApplicationSolution/Attribute:as_user_number' => 'Number of users',
+	'Class:ApplicationSolution/Attribute:as_user_type' => 'Type of users',
+));
+?>
---
+++ schirrms-as-addfields/fr.dict.schirrms-as-addfields.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'French', array(
+	// Dictionary entries go here
+	'Class:ApplicationSolution/Attribute:as_cos' => 'Contrat de service',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:247' => '24 heures / 24 - 7 jours / 7',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:715' => 'lundi au vendredi, 7h15 à 19h15',
+	'Class:ApplicationSolution/Attribute:as_cos/Value:830' => 'lundi au vendredi, 8h30 à 18h30',
+	'Class:ApplicationSolution/Attribute:as_domain' => 'Domaine fonctionel',
+	'Class:ApplicationSolution/Attribute:as_end_date' => 'Date de fin de service',
+	'Class:ApplicationSolution/Attribute:as_last_update' => 'Date de dernière mise à jour',
+	'Class:ApplicationSolution/Attribute:as_nature' => 'Nature',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:mixed' => 'Mixte',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:partner' => 'Partenaire',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:package' => 'Progiciel',
+	'Class:ApplicationSolution/Attribute:as_nature/Value:specific' => 'Spécifique',
+	'Class:ApplicationSolution/Attribute:as_state' => 'État',
+	'Class:ApplicationSolution/Attribute:as_state/Value:undefined' => 'À clarifier',
+	'Class:ApplicationSolution/Attribute:as_state/Value:decommissioning' => 'À décommissionner',
+	'Class:ApplicationSolution/Attribute:as_state/Value:decommissioned' => 'Décommissionnée',
+	'Class:ApplicationSolution/Attribute:as_state/Value:production' => 'En production',
+	'Class:ApplicationSolution/Attribute:as_state/Value:external' => 'Externe',
+	'Class:ApplicationSolution/Attribute:as_state/Value:future' => 'À venir',
+	'Class:ApplicationSolution/Attribute:as_type' => 'Type d\'usage',
+	'Class:ApplicationSolution/Attribute:as_type/Value:job' => 'Métier',
+	'Class:ApplicationSolution/Attribute:as_type/Value:technical' => 'Technique',
+	'Class:ApplicationSolution/Attribute:as_type/Value:a10' => 'Sur A10',
+	'Class:ApplicationSolution/Attribute:as_user_number' => 'Nombre d\'utilisateurs',
+	'Class:ApplicationSolution/Attribute:as_user_type' => 'Type d\'utilisateurs',
+));
+?>
